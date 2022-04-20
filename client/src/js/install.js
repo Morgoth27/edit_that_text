61ba0bf6c9d2db88dca0f58661953f49ac7e0ed4
Tiffany <leopardmitai@gmail.com>
2022-04-20 15:18:35 -0700

    switch to deferredPrompt

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,24 +3,24 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.heldPrompt = event;
+    window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
 
 //  a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const eventPopup = window.heldPrompt;
+    const eventPopup = window.deferredPrompt;
     if (!eventPopup) { return }
     eventPopup.prompt();
 
-    window.heldPrompt = undefined;
-    // delete(heldPrompt)
+    window.deferredPrompt = null;
+    // delete(deferredPrompt)
 
     butInstall.classList.toggle('hidden', true);
 });
 
 // an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.heldPrompt = undefined;
-    // delete(heldPrompt)
+    window.deferredPrompt = null;
+    // delete(deferredPrompt)
 });
